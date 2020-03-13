24f57a28939f5c7dd0dff71639c236ff0947f3df
RVohmin <vohmin77@gmail.com>
2020-03-13 21:43:42 +0300

    1. Принципы Kiss, Dry и Yagni[#256218] new tests

--- chapter_001/src/main/java/ru/job4j/kiss/MaxMin.java
+++ chapter_001/src/main/java/ru/job4j/kiss/MaxMin.java
@@ -5,7 +5,14 @@ import java.util.List;
 import java.util.function.Supplier;
 
 public class MaxMin {
-
+    /**
+     * This function used by methods min() & max()
+     * @param value - List
+     * @param comparator - (x, y) -> x.compareTo(y)
+     * @param initValue -1 then finding max or 1 then finding min (Supplier<Integer> initValue)
+     * @param <T> Type
+     * @return Maxmin
+     */
     private <T> T solveMinMax(List<T> value, Comparator<T> comparator, Supplier<Integer> initValue) {
         T rsl = value.get(0);
         for (T item : value) {
@@ -15,6 +22,14 @@ public class MaxMin {
         }
         return rsl;
     }
+
+    /**
+     *
+     * @param value List
+     * @param comparator Comparator
+     * @param <T> Type value
+     * @return solved min or max
+     */
     public <T> T max(List<T> value, Comparator<T> comparator) {
         Supplier<Integer> initValue = () -> -1;
         return solveMinMax(value, comparator, initValue);
@@ -26,7 +41,7 @@ public class MaxMin {
     }
 
     public static void main(String[] args) {
-        List<Integer> list = List.of(79, 69, 49, 19, 22, 34, 5, 66);
+        List<Integer> list = List.of(79, 69, 49, 19, 19, 21, 22, 23, 34, 5, 66);
         MaxMin obj = new MaxMin();
         System.out.println(obj.max(list, Integer::compareTo));
         System.out.println(obj.min(list, Integer::compareTo));
--- chapter_001/src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ chapter_001/src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -10,19 +10,39 @@ import static org.junit.Assert.assertThat;
 
 
 public class MaxMinTest {
-    List<Integer> list = List.of(79, 69, 49, 19, 22, 34, 5, 66);
-    MaxMin obj = new MaxMin();
+
 
     @Test
     public void thenMin5ThenTrue() {
+        List<Integer> list = List.of(79, 69, 49, 19, 5, 22, 34, 5, 66);
+        MaxMin obj = new MaxMin();
         Integer expected = 5;
+        Integer rsl = obj.min(list, Integer::compareTo);
+        assertThat(expected, is(rsl));
+    }
+
+    public void thenMinNotMinus5ThenTrue() {
+        List<Integer> list = List.of(79, 69, 49, 19, -5, 22, 34, 5, 66);
+        MaxMin obj = new MaxMin();
+        Integer expected = -5;
         Integer rsl = obj.min(list, Integer::compareTo);
         assertThat(expected, is(rsl));
     }
 
     @Test
     public void thenMax79ThenTrue() {
+        List<Integer> list = List.of(79, 69, 49, 19, 5, 22, 34, 5, 66);
+        MaxMin obj = new MaxMin();
         Integer expected = 79;
+        Integer rsl = obj.max(list, Integer::compareTo);
+        assertThat(expected, is(rsl));
+    }
+
+    @Test
+    public void thenMax100ThenTrue() {
+        List<Integer> list = List.of(79, 69, 49, 19, 5, 22, 34, 5, 66, 100);
+        MaxMin obj = new MaxMin();
+        Integer expected = 100;
         Integer rsl = obj.max(list, Integer::compareTo);
         assertThat(expected, is(rsl));
     }
