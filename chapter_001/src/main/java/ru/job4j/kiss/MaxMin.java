ccf0206dce2f520885e5f9a87e885cdb26feef62
RVohmin <vohmin77@gmail.com>
2020-03-14 08:57:09 +0300

    1. Принципы Kiss, Dry и Yagni[#256218] переделано на BiP redicate

--- chapter_001/src/main/java/ru/job4j/kiss/MaxMin.java
+++ chapter_001/src/main/java/ru/job4j/kiss/MaxMin.java
@@ -2,21 +2,21 @@ package ru.job4j.kiss;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiPredicate;
 import java.util.function.Supplier;
 
 public class MaxMin {
     /**
      * This function used by methods min() & max()
      * @param value - List
-     * @param comparator - (x, y) -> x.compareTo(y)
-     * @param initValue -1 then finding max or 1 then finding min (Supplier<Integer> initValue)
+     * @param predict - BiPredicate
      * @param <T> Type
      * @return Maxmin
      */
-    private <T> T solveMinMax(List<T> value, Comparator<T> comparator, Supplier<Integer> initValue) {
+    private <T> T solveMinMax(List<T> value, BiPredicate<T, T> predict) {
         T rsl = value.get(0);
         for (T item : value) {
-            if (comparator.compare(rsl, item) == initValue.get()) {
+            if (predict.test(rsl, item)) {
                 rsl = item;
             }
         }
@@ -31,20 +31,18 @@ public class MaxMin {
      * @return solved min or max
      */
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        Supplier<Integer> initValue = () -> -1;
-        return solveMinMax(value, comparator, initValue);
+        return solveMinMax(value, (left, right) -> comparator.compare(left, right) <= 0);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        Supplier<Integer> initValue = () -> 1;
-        return solveMinMax(value, comparator, initValue);
+        return solveMinMax(value, (left, right) -> comparator.compare(left, right) >= 0);
     }
 
-//    public static void main(String[] args) {
-//        List<Integer> list = List.of(79, 69, 49, 19, 19, 21, 22, 23, 34, 5, 66);
-//        MaxMin obj = new MaxMin();
-//        System.out.println(obj.max(list, Integer::compareTo));
-//        System.out.println(obj.min(list, Integer::compareTo));
-//
-//    }
+    public static void main(String[] args) {
+        List<Integer> list = List.of(-288, 49, 79, 69, 49, 19, 19, 79, 21, 22, 23, 34, 5, 66);
+        MaxMin obj = new MaxMin();
+        System.out.println(obj.max(list, Integer::compareTo));
+        System.out.println(obj.min(list, Integer::compareTo));
+
+    }
 }
