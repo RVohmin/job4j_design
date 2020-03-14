934b5be506d7415d8d94e5d140a1491e119e2aba
RVohmin <vohmin77@gmail.com>
2020-03-14 23:45:04 +0300

    3. Исправить код класс SimpleGenerator.[#256220]

--- chapter_001/src/main/java/ru/job4j/kiss/MaxMin.java
+++ chapter_001/src/main/java/ru/job4j/kiss/MaxMin.java
@@ -3,7 +3,6 @@ package ru.job4j.kiss;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.BiPredicate;
-import java.util.function.Supplier;
 
 public class MaxMin {
     /**
