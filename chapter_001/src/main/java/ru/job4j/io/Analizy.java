b4d3f9ef2f821f871ceb0504b33256e1ae1c48da
RVohmin <vohmin77@gmail.com>
2020-03-21 20:12:08 +0300

    2. Анализ доступности сервера.[#256205] remaked loop, and now read and write on one loop without using memory

--- chapter_001/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_001/src/main/java/ru/job4j/io/Analizy.java
@@ -1,13 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * ru.job4j.io.Analizy
