664aa1742a5201d482fc0244fec214263ef245f9
RVohmin <vohmin77@gmail.com>
2020-04-03 17:51:06 +0300

    5.1.1. Итератор для двухмерного массива int[][][#256172]

--- chapter_001/src/main/java/ru/job4j/collections/iterator/MatrixIterator.java
+++ chapter_001/src/main/java/ru/job4j/collections/iterator/MatrixIterator.java
@@ -9,14 +9,46 @@ import java.util.Iterator;
  * @version 1
  * @since 02.04.2020
  */
-public class MatrixIterator implements Iterable<Integer> {
+public class MatrixIterator<T> implements Iterator<T> {
+    private int size;          //всего элементов в матрице
+    private int position = 0;  //позиция текущего элемента для "выдачи"
+    private int row = 0;       //строка текущего элемента
+    private int col = 0;       //столбец текущего элемента
+    private T[][] matrix;
+
+    public MatrixIterator(T[][] matrix) {
+        this.matrix = matrix;
+        this.size = countElements(matrix);
+    }
+
     /**
-     * Returns an iterator over elements of type {@code T}.
-     *
-     * @return an Iterator.
+     * Метод подсчета всех элементов в двумерной матрице
+     * @param matrix - двумерный массив
+     * @return количество всех элементов
      */
+    private int countElements(T[][] matrix) {  //считаем количество элементов в матрице
+        int count = 0;
+        for (T[] row : matrix) {
+            count += row.length;
+        }
+        return count;
+    }
+
     @Override
-    public Iterator<Integer> iterator() {
-        return null;
+    public boolean hasNext() {
+        return position < size;
+    }
+
+    @Override
+    public T next() {
+        T element = matrix[row][col];  //запоминаем текущий элемент
+        //переходим к следующему элементу
+        position++;
+        col++;
+        while (row < matrix.length && col >= matrix[row].length) { //для того, чтоб пропустить возможные "пустые" строки
+            col = 0;
+            row++;
+        }
+        return element;
     }
 }
