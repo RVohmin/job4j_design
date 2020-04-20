ebc530906158ef7f14ef67a6bf26b4515a8af2c9
RVohmin <vohmin77@gmail.com>
2020-04-20 19:34:59 +0300

    восстановлен репозиторий!

--- chapter_002/src/main/java/ru/job4j/io/zip/FindMask.java
+++ chapter_002/src/main/java/ru/job4j/io/zip/FindMask.java
@@ -7,7 +7,7 @@ package ru.job4j.io.zip;
  * @version 1
  * @since 01.04.2020
  */
-public class FindMask implements FindAble {
+public class FindMask implements Find {
 
     @Override
     public String getFileMask(String arg) {
