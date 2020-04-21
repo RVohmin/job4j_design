88cb46654d2cd21aa6f4a7981e7b6be22a22d27a
RVohmin <vohmin77@gmail.com>
2020-04-21 12:33:52 +0300

    1. Создать модель User[#256181]

---
+++ chapter_003/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,16 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * job4j_design ru.job4j.map.User
+ *
+ * @author romanvohmin
+ * @since 21.04.2020 12:32
+ */
+public class User {
+    String name;
+    int children;
+    Calendar birthday;
+
+}
