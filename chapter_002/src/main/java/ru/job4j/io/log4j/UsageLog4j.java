25a9df0626ce3909c4d3067fe3ff6623fc3ee49a
RVohmin <vohmin77@gmail.com>
2020-06-04 10:28:33 +0300

    2. Simple Loggin Facade 4 Java.

--- chapter_002/src/main/java/ru/job4j/io/log4j/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/log4j/UsageLog4j.java
@@ -8,10 +8,22 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        byte age1 = 42;
+        short age2 = 43;
+        long age3 = 120;
+        float age4 = 100.0f;
+        double age5 = 12.5d;
+        char nameLast = 'A';
+        boolean bool = true;
+
+        LOG.debug("User info name : {}, age : {}", nameLast, age1);
+        LOG.debug("User info name : {}, age : {}", bool, age2);
+        LOG.debug("User info  {}", age3);
+        LOG.debug("User info name : {}, age : {}", nameLast, age4);
+        LOG.debug("User info name : {}, age : {}", nameLast, age5);
+
     }
 }
